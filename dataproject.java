66b988b4b4d4b99099da2c40034de50d87d997a8
jagalletta <jagalletta@gmail.com>
2018-03-02 11:49:19 -0500

    First Commit - Made TODO list

---
+++ dataproject.java
@@ -0,0 +1,18 @@
+public class dataproject	{
+
+	public static void main (String[] args)	{
+		//Starting with basic basic basic setup, building from ABSOLUTE ZERO.
+		//Will be pushing new commits as this evolves!  :)
+
+		System.out.println("Welcome to my data project!");
+		System.out.println("This program will accept the project-data.txt file as input");
+		System.out.println("and display its contents grouped by their color attributes.");
+
+		//TODO prompt user for input file path
+		//TODO accept input in the form of .txt file.
+		//TODO parse file contents into dataItem objects with attributes Date, Name, Company, and Color
+		//NOTE Assumption is made that all entries are unique.
+		//TODO as objects are created insert into [sorted array or linked list/binary tree (still deciding - need to review)]
+		//TODO traverse data structure to output items with some fancy formatting.
+	}
+}
